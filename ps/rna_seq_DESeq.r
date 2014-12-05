6ceed11f9b27eace0cb3268f417e605d25840348
schimar <martin.schilling@usu.edu>
2014-12-05 09:51:00 -0700

    read sam files

--- ps/rna_seq_DESeq.r
+++ ps/rna_seq_DESeq.r
@@ -4,5 +4,14 @@ library(GenomicRanges)
 
 ####
 
+c19 <- read.delim("C268_19c.sam", header= F)
+c20 <- read.delim("C268_20c.sam", header= F)
+c21 <- read.delim("C268_20c.sam", header= F)
+c22 <- read.delim("C268_20c.sam", header= F)
+c23<- read.delim("C268_20c.sam", header= F)
+c24 <- read.delim("C268_20c.sam", header= F)
+
+
+
 
 
